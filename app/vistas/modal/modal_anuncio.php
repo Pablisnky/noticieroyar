2a3650dc83e98c38f2517cb2418f4c74190ceea2
PaCabeza <pcabeza7@gmail.com>
2022-10-06 20:30:20 -0400

    cambios generales

---
+++ app/vistas/modal/modal_anuncio.php
@@ -0,0 +1,9 @@
+
+    <!-- VENTANA MODAL -->
+	<!-- <div class="cont_modal" id="VentanaModal">				
+		<label class="cont_modal--cerrar Default_pointer" id="Cerrar--modal"><span class="material-icons-outlined">cancel</span></label>
+		<h1 class="cont_modal--h1">www.noticieroyaracuy.com</h1>
+		<p class="cont_modal--p">Sitio web en construcción.</p>
+		<img class="cont_modal--logo" src="<?php echo RUTA_URL?>/public/images/imprenta.jpg"/>
+		<p class="cont_modal--label">A partir del 10-10-2022</p>
+	</div> -->
--- app/vistas/view/inicio_V.php
+++ app/vistas/view/inicio_V.php
@@ -7,15 +7,6 @@
         <img class="imagen_5 imagen_5--inicio" alt="Logo PWA" src="<?php echo RUTA_URL;?>/public/images/PWA.png"/>
     </div> -->
     
-    <!-- VENTANA MODAL -->
-	<!-- <div class="cont_modal" id="VentanaModal">				
-		<label class="cont_modal--cerrar Default_pointer" id="Cerrar--modal"><span class="material-icons-outlined">cancel</span></label>
-		<h1 class="cont_modal--h1">www.noticieroyaracuy.com</h1>
-		<p class="cont_modal--p">Sitio web en construcción.</p>
-		<img class="cont_modal--logo" src="<?php echo RUTA_URL?>/public/images/imprenta.jpg"/>
-		<p class="cont_modal--label">A partir del 10-10-2022</p>
-	</div> -->
-
 	<!-- PORTADA -->
 	<div class="cont_portada" id="Cont_Portada">
         <?php
